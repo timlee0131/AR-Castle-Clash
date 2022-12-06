68663e8116cc3062456662a6f50bdbf0dbf34ce5
Arthur Zhang <60437435+yunchengzhang1@users.noreply.github.com>
2022-12-06 12:53:38 -0600

    ar stuff

---
+++ ARKitInteraction/ViewController+ARSCNViewDelegate.swift
@@ -0,0 +1,142 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+ARSCNViewDelegate interactions for `ViewController`.
+*/
+
+import ARKit
+import SceneKit
+
+extension ViewController: ARSCNViewDelegate, ARSessionDelegate {
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+        let isAnyObjectInView = virtualObjectLoader.loadedObjects.contains { object in
+            return sceneView.isNode(object, insideFrustumOf: sceneView.pointOfView!)
+        }
+        
+        DispatchQueue.main.async {
+            self.updateFocusSquare(isObjectVisible: isAnyObjectInView)
+            
+            // If the object selection menu is open, update availability of items
+            if self.objectsViewController?.viewIfLoaded?.window != nil {
+                self.objectsViewController?.updateObjectAvailability()
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        DispatchQueue.main.async {
+            self.statusViewController.cancelScheduledMessage(for: .planeEstimation)
+            self.statusViewController.showMessage("SURFACE DETECTED")
+            if self.virtualObjectLoader.loadedObjects.isEmpty {
+                self.statusViewController.scheduleMessage("TAP + TO PLACE AN OBJECT", inSeconds: 7.5, messageType: .contentPlacement)
+            }
+        }
+        if(!plane_added){
+            /// plane visualize
+            // Place content only for anchors found by plane detection.
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+            
+            // Create a custom object to visualize the plane geometry and extent.
+            let plane = Plane(anchor: planeAnchor, in: sceneView)
+            
+            // Add the visualization to the ARKit-managed node so that it tracks
+            // changes in the plane anchor as plane estimation continues.
+            node.addChildNode(plane)
+            plane_added = true
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        updateQueue.async {
+            if let objectAtAnchor = self.virtualObjectLoader.loadedObjects.first(where: { $0.anchor == anchor }) {
+                objectAtAnchor.simdPosition = anchor.transform.translation
+                objectAtAnchor.anchor = anchor
+            }
+        }
+        
+        if(!plane_locked){
+            /// plane visualize
+            // Update only anchors and nodes set up by `renderer(_:didAdd:for:)`.
+            guard let planeAnchor = anchor as? ARPlaneAnchor,
+                let plane = node.childNodes.first as? Plane
+                else { return }
+            
+            // Update ARSCNPlaneGeometry to the anchor's new estimated shape.
+            if let planeGeometry = plane.meshNode.geometry as? ARSCNPlaneGeometry {
+                planeGeometry.update(from: planeAnchor.geometry)
+            }
+
+            // Update extent visualization to the anchor's new bounding rectangle.
+            if let extentGeometry = plane.extentNode.geometry as? SCNPlane {
+                extentGeometry.width = CGFloat(planeAnchor.extent.x)
+                extentGeometry.height = CGFloat(planeAnchor.extent.z)
+                plane.extentNode.simdPosition = planeAnchor.center
+            }
+        }
+        
+    }
+    
+
+
+    
+    
+    /// - Tag: ShowVirtualContent
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        statusViewController.showTrackingQualityInfo(for: camera.trackingState, autoHide: true)
+        switch camera.trackingState {
+        case .notAvailable, .limited:
+            statusViewController.escalateFeedback(for: camera.trackingState, inSeconds: 3.0)
+        case .normal:
+            statusViewController.cancelScheduledMessage(for: .trackingStateEscalation)
+            showVirtualContent()
+        }
+    }
+
+    func showVirtualContent() {
+        virtualObjectLoader.loadedObjects.forEach { $0.isHidden = false }
+    }
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
+        }
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Hide content before going into the background.
+        hideVirtualContent()
+    }
+    
+    /// - Tag: HideVirtualContent
+    func hideVirtualContent() {
+        virtualObjectLoader.loadedObjects.forEach { $0.isHidden = true }
+    }
+
+    /*
+     Allow the session to attempt to resume after an interruption.
+     This process may not succeed, so the app must be prepared
+     to reset the session if the relocalizing status continues
+     for a long time -- see `escalateFeedback` in `StatusViewController`.
+     */
+    /// - Tag: Relocalization
+    func sessionShouldAttemptRelocalization(_ session: ARSession) -> Bool {
+        return true
+    }
+}
